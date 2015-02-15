4a7d8f41aa14369d3f6496ecba4f3a36b40c0f96
James Knepper <knepperjb@gmail.com>
2015-02-15 23:34:39 +0000

    modify application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  def after_sign_up_path_for(resource)
+    # after_sign_in_path_for(resource)
+    '/set-location'
+  end
 
   protected
 
@@ -18,10 +22,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :goal, :email, :password, :description, :level, :password_confirmation, :current_password) }
   end
 
-  def after_sign_up_path_for(resource)
-    # after_sign_in_path_for(resource)
-    '/set-location'
-  end
 
   
 end
