59f524ce5f1e43c51e3494a44bfe60c36c96c4b2
James Knepper <knepperjb@gmail.com>
2015-02-18 05:59:50 +0000

    username presence is true

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates_uniqueness_of :username
+  validates :username, :presence => true, :uniqueness => true
   has_many :messages
   has_many :subscriptions
   has_many :workouts
